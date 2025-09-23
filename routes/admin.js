902089640ec62f1e240764aad308508eafa9ae1c
richard <peliritter7@gmail.com>
2025-09-23 19:14:33 +0300

    Added Product Schema

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const user = require("../models/users");
-const category = require("../models/categoryModel");
-const proucts = require("../models/productsSchema");
-const { ensureAdmin } = require("../AuthMiddleWare/checkRole");
 
 router.get("/admin-page", ensureAdmin, async (req, res) => {
   res.render("admin-dashboard");
 });
 
-router.post("/admin-page", ensureAdmin, async (req, res) => {});
+// router.post("/admin-page", ensureAdmin, async (req, res) => {});
 
 module.exports = router;
